864a9c3f5674c5ea342cafde76a76d088bc718b0
SluvsP <salomekbg@gmail.com>
2016-10-14 16:20:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,23 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "views/pirates") }
 
-    # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
 
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+        params[:pirate][:ships].each do |ship|
+          Ship.new(ship)
+        end
+      @ship_array = Ship.all
+      erb :show
+    end
   end
 end
